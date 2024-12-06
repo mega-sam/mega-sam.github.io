f195a9de43741315effadc5e95738f758c318324
Aleksander Hołyński <alik.holynski@gmail.com>
2024-12-05 16:01:07 -0800

    updates

--- static/js/index.js
+++ static/js/index.js
@@ -1,14 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
   var options = {
-    slidesToScroll: 2,
-    slidesToShow: 2,
-    loop: true,
-    infinite: false,
-    pagination: false,
-    autoplay: true,
-    autoplaySpeed: 5000,
+			slidesToScroll: 1,
+			slidesToShow: 3,
+			loop: true,
+			infinite: true,
+			autoplay: false,
+			autoplaySpeed: 3000,
   };
 
   // Initialize all div with carousel class
   var carousels = bulmaCarousel.attach('.carousel', options);
 });
+
+window.HELP_IMPROVE_VIDEOJS = false;
+
